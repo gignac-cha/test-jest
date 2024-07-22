4b9bea45da717a3763f51bc9d84185b039c4c0ba
Jin-Hyeok Cha <38489680+gignac-cha@users.noreply.github.com>
2024-07-22 14:34:32 +0900

    test

--- sources/random.ts
+++ sources/random.ts
@@ -1,5 +1,5 @@
-export const random(x?: number, y?: number) => {
-    const random = Math.random();
+export const random = (x?: number, y?: number) => {
+    const r = Math.random();
     if (typeof x === 'number' && typeof y === 'number') {
         return Math.floor(x + r * (y - x));
     } else if (typeof x === 'number') {
@@ -8,11 +8,11 @@ export const random(x?: number, y?: number) => {
     return r;
 };
 
-export const choice<T>(array: T[]): T => {
+export const choice = <T>(array: T[]): T => {
     return array[random(array.length)];
 };
 
-export const shuffle<T>(array: T[]): T[] => {
+export const shuffle = <T>(array: T[]): T[] => {
     for (let i = 0; i < array.length; ++i) {
         const j = random(array.length);
         [array[i], array[j]] = [array[j], array[i]];
