bf57397627ce4683ec57078c532b2a05861b4117
Jin-Hyeok Cha <38489680+gignac-cha@users.noreply.github.com>
2024-07-22 14:56:17 +0900

    test

--- jest.config.ts
+++ jest.config.ts
@@ -2,11 +2,14 @@ import type { Config } from 'jest';
 
 export default async (): Promise<Config> => {
     return {
-        "bail": 1,
-        "collectCoverage": true,
-        "coverageDirectory": "coverage",
-        "coverageProvider": "v8",
-        "testEnvironment": "node",
-        "verbose": true
+        bail: 1,
+        collectCoverage: true,
+        coverageDirectory: 'coverage',
+        coverageProvider: 'v8',
+        testEnvironment: 'node',
+        transform: {
+          '\\.[jt]sx?$': '@swc/jest',
+        },
+        verbose: true
     };
 };
--- tests/random.test.ts
+++ tests/random.test.ts
@@ -4,19 +4,19 @@ describe('Random', () => {
     test('random()', () => {
         const value = random();
         expect(value).toBeGreaterThanOrEqual(0);
-        expect(value).toBeLessThen(1);
+        expect(value).toBeLessThan(1);
     });
     test('random(x)', () => {
         const x = 100;
         const value = random(x);
         expect(value).toBeGreaterThanOrEqual(0);
-        expect(value).toBeLessThen(x);
+        expect(value).toBeLessThan(x);
     });
     test('random(x, y)', () => {
         const x = 100;
         const y = 200;
         const value = random(x, y);
         expect(value).toBeGreaterThanOrEqual(x);
-        expect(value).toBeLessThen(y);
+        expect(value).toBeLessThan(y);
     });
 });
